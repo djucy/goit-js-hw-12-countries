d393f349c10b6b07542191f255f869cb76ae92a5
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2021-11-05 17:59:07 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,18 @@
 
 export default { fetchCountries };
+import { alert, defaultModules } from '@pnotify/core/dist/PNotify.js';
 
 
 function fetchCountries(searchQuery) {
     return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
-        .then(response => response.json(),
-        )
-        
-        
-};
+        .then(response => {
+            if (response.ok) {
+    return response.json();
+            }
+            else {throw new Error(`HTTP error! status: ${response.status}`);  
+            }
+            })
+            };
 
 
+ 
